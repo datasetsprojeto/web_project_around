0282f303b4dd62bc45156559bafedd87fb7e9873
RenatoSP <soares.6.renato@gmail.com>
2024-10-19 20:51:02 -0300

    feat: Popup-added

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,9 @@ const nameInput = document.querySelector("#input__name");
 const jobInput = document.querySelector("#input__job");
 const profileName = document.querySelector(".header__name");
 const profileJob = document.querySelector(".header__job");
+const titleInput = document.querySelector("#input__title");
+const linkInput = document.querySelector("#input__job");
+
 
 //abre modal
 function openModal() {
@@ -15,6 +18,16 @@ function closeModal() {
   document.getElementById('modal').style.display = 'none';
 }
 
+//abre popup
+function openPopup() {
+  const openPopup = document.getElementById('.popup');
+  document.getElementById('popup').style.display = 'flex';
+}
+
+function closePopup() {
+  const closePopup = document.getElementById('.popup');
+  document.getElementById('popup').style.display = 'none';
+}
 
 // valida e aatualiza as informações do perfil
 function handleProfileFormSubmit(evt) {
@@ -27,4 +40,3 @@ function handleProfileFormSubmit(evt) {
 }
 
 formElement.addEventListener("submit", handleProfileFormSubmit);
-
