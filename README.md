e298cd8afdc12fc04b25062e6f84f5f0a6c0e81a
RenatoSP <soares.6.renato@gmail.com>
2024-10-28 21:27:02 -0300

    feat: create cards and updates

--- README.md
+++ README.md
@@ -4,6 +4,8 @@
 
 # Neste projeto, foram utilizadas as tecnologias de HTML, CSS e JavaScript. Utilizando de Flex-Box, Grid-Box e DOM como principais funções para trazer funcionalidade e realidade ao layout designado pelo figma e roteiro.
 
-# Atualização: Nesse projeto, a maneira pela qual as imagens seram carregadas será atualizadas via JavaScript.
+#  Foi feito a atualização de maneira pela qual as imagens fossem carregadas e atualizadas via JavaScript.
+
+# Atualização: Que seja implementado outras formas mais seguras desde a entrada de dados até o salvamento destes
 
 # Link do projeto: https://datasetsprojeto.github.io/web_project_around/
--- pages/index.js
+++ pages/index.js
@@ -1,35 +1,88 @@
 const formElement = document.querySelector("#modal__form");
 const nameInput = document.querySelector("#input__name");
 const jobInput = document.querySelector("#input__job");
+
 const profileName = document.querySelector(".header__name");
 const profileJob = document.querySelector(".header__job");
+
+const popupForm = document.querySelector(".popup__form");
 const titleInput = document.querySelector("#input__title");
-const linkInput = document.querySelector("#input__job");
+const linkInput = document.querySelector("#input__link-image");
+const addCard = document.querySelector("#popup__button");
 
 
-//abre modal
+const containerCards = document.querySelector(".main__cards");
+
+const cardElement = document.querySelector(".main__card");
+const image = document.querySelector(".main__image");
+const titleImage = document.querySelector(".main__title-image");
+const heart = document.querySelector('#main__icon');
+
+const expandedPopup = document.querySelector(".popup-expanded");
+const expandedImage = document.querySelector(".popup-expanded__image");
+const titleExpandedImage = document.querySelector(".popup-expanded__image-name");
+const closeExpandedPopup = document.querySelector("#popup-expanded__button-close");
+
+const initialCards = [
+  {
+    name: "Lago di Braies",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_lago.jpg",
+  },
+  {
+    name: "Parque Nacional da Vanoise",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_vanoise.jpg",
+  },
+  {
+    name: "Latemar",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_latemar.jpg",
+  },
+  {
+    name: "Montanhas Carecas",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_bald-mountains.jpg",
+  },
+  {
+    name: "Lago Louise",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_lake-louise.jpg",
+  },
+  {
+    name: "Vale de Yosemite",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_yosemite.jpg",
+  },
+];
+
+
+
+//opeen modal
 function openModal() {
   const openModal = document.getElementById('.modal');
   document.getElementById('modal').style.display = 'flex';
 }
 
+//close modal
 function closeModal() {
   const closeModal = document.getElementById('.modal');
   document.getElementById('modal').style.display = 'none';
 }
 
-//abre popup
+//open popup
 function openPopup() {
   const openPopup = document.getElementById('.popup');
   document.getElementById('popup').style.display = 'flex';
 }
 
+//close popup
 function closePopup() {
   const closePopup = document.getElementById('.popup');
   document.getElementById('popup').style.display = 'none';
 }
 
-// valida e aatualiza as informações do perfil
+//close popup Image
+function closePopupImage() {
+  expandedPopup.classList.remove('popup-expanded_oppened');
+}
+
+
+// valida e atualiza as informações do perfil
 function handleProfileFormSubmit(evt) {
   evt.preventDefault();
   if (nameInput.value != "" && jobInput.value != "") {
@@ -40,3 +93,55 @@ function handleProfileFormSubmit(evt) {
 }
 
 formElement.addEventListener("submit", handleProfileFormSubmit);
+
+// criando um novo card
+function createCard(card) {
+
+  const cardTemplate = document.querySelector("#main__template").content;
+  const cardElement = cardTemplate.querySelector(".main__card").cloneNode(true);
+
+  cardElement.querySelector(".main__title-image").textContent = card.name;
+  cardElement.querySelector(".main__image").setAttribute("src", card.link);
+  cardElement.querySelector(".main__image").setAttribute("alt", card.name);
+  cardElement.querySelector(".main__icon").addEventListener('click', function (event) {
+    event.target.classList.toggle("main__icon_black-heart");
+  });
+  cardElement.querySelector("#main__image").addEventListener("click", function (event) {
+    console.log("Clicou na imagem duas vezes");
+    expandedPopup.querySelector(".popup-expanded__image").setAttribute("src", card.link);
+    console.log(expandedPopup.querySelector(".popup-expanded__image"));
+    expandedPopup.querySelector(".popup-expanded__image-name").textContent = card.name;
+    console.log(expandedPopup.querySelector(".popup-expanded__image-name"));
+    expandedPopup.classList.add('popup-expanded_oppened');
+    console.log(expandedPopup.classList.add('popup-expanded_oppened'));
+  });
+  cardElement.querySelector(".main__button-delete").addEventListener("click", function (event) {
+    event.target.parentElement.remove();
+  })
+  return cardElement
+}
+for (const card of initialCards) {
+  const newCard = createCard(card);
+  containerCards.prepend(newCard);
+}
+
+function addNewImageCard(evt) {
+  evt.preventDefault()
+  if (titleInput.value != "" && linkInput.value != "") {
+    const newCards = createCard({
+      name: titleInput.value,
+      link: linkInput.value,
+    })
+    containerCards.prepend(newCards);
+    titleImage.value = "";
+    linkInput.value = "";
+  }
+}
+
+popupForm.addEventListener("submit", addNewImageCard);
+
+
+
+
+
+
